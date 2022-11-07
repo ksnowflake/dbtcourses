7fb6ac0615df2658e46be8aa8e5713a84fadfe82
Kara Radecki <kplaggemars1@gmail.com>
2022-11-07 16:33:21 +0000

    Added  orders_by_day.sql file to analyses folder

---
+++ analyses/orders_by_day.sql
@@ -0,0 +1,28 @@
+WITH orders AS (
+    SELECT * FROM {{ ref('stg_orders') }}
+),
+
+daily AS (
+    SELECT
+        order_date,
+        COUNT(*) AS num_orders,
+
+    {% for order_status in ['returned', 'completed', 'return_pending', 'shipped', 'placed'] %}
+        
+        SUM(CASE WHEN status = '{{ order_status }}' THEN 1 ELSE 0 END) AS {{ order_status}}_total {{ ',' if not loop.last}}
+
+    {% endfor %}
+
+    FROM
+        orders
+    GROUP BY 1
+),
+
+compared AS (
+    SELECT *,
+        lag(num_orders) OVER (ORDER BY order_date) AS previous_day_orders
+    FROM
+        daily
+)
+
+SELECT * FROM compared
